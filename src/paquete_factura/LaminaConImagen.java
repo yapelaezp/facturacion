4686e6dfbb938205d7ee04273391d190d1b04d29
yapelaezp <yapelaezp@unal.edu.co>
2022-01-05 13:37:05 -0500

    Uploading software app

---
+++ src/paquete_factura/LaminaConImagen.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete_factura;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author satur
+ */
+public class LaminaConImagen extends JPanel{
+    
+     Image Imagen;
+    public void paintComponent(Graphics g){
+     
+        super.paintComponent(g);
+                File miImagen= new File("src/Graficos/Shen.jpg");
+                try{
+                    Imagen = ImageIO.read(miImagen);
+                }       
+                catch(IOException e){
+                    System.out.println("La imagen no se encuentra");
+                    
+                }
+                g.drawImage(Imagen, 0, 0, null);
+    }
+    
+}
